5f4587020f17c19672ca3276f7cc9fd4abcc922a
Gou <13408696398@163.com>
2021-10-06 11:51:47 +0800

    Initial commit

---
+++ ssm_service/src/main/java/com/jiexi/service/impl/UserServiceImpl.java
@@ -0,0 +1,49 @@
+package com.jiexi.service.impl;
+
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import com.jiexi.dao.UserDao;
+import com.jiexi.domain.User;
+import com.jiexi.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+@Service
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    private UserDao userDao;
+
+    @Override
+    public boolean save(User user) {
+        return userDao.save(user);
+    }
+
+    @Override
+    public boolean update(User user) {
+        return userDao.update(user);
+    }
+
+    @Override
+    public boolean delete(Integer uuid) {
+        return userDao.delete(uuid);
+    }
+
+    @Override
+    public User get(Integer uuid) {
+        return userDao.get(uuid);
+    }
+
+    @Override
+    public PageInfo<User> getAll(int page,int size) {
+        PageHelper.startPage(page,size);
+        List<User> all = userDao.getAll();
+        return new PageInfo<User>(all);
+    }
+
+    @Override
+    public User login(String userName, String password) {
+        return userDao.getByUserNameAndPassword(userName,password);
+    }
+}
